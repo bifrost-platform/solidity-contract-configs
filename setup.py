d2166c58d5b9255518e4cbeacff57d5f5d6e3f5b
dongchangYoo <yoodc8167@gmail.com>
2023-01-04 21:30:30 +0900

    BN-873 fix typo in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from bridgeconst.__init__ import __version__
 
 setup(
-    name="bridgeconsts",
+    name="bridgeconst",
     version=__version__,
-    packages=["bridgeconsts"],
+    packages=["bridgeconst"],
 )
